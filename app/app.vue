a5de37d4ec8ba1048fd231af6a6f60e98ae4dc6d
Montaño <jorge.alan.montano@gmail.com>
2026-02-20 17:53:13 -0600

    Proyecto Nuxt inicial

---
+++ app/app.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="tarjeta">
+
+  <img src="/images/perfil.jpg" class="foto-perfil" />
+
+  <h1 class="nombre">Jorge Montaño</h1>
+  <h2>Ingeniero en TICS</h2>
+    <p class="descripcion">
+      Especialista en desarrollo web.
+    </p>
+
+    <div class="contacto">
+      <p>📧 jorge.alan.montano@gmial.com </p>
+      <p>📱 +52 5612791223</p>
+    </div>
+
+    <div class="redes">
+  <a href="#">GitHub</a>
+  <a href="#">LinkedIn</a>
+  <a href="#">Correo</a>
+</div>
+
+    <div class="botones">
+      <button @click="saludar">Contacto</button>
+
+      <button @click="toggleTheme">
+        {{ darkMode ? '☀️ Modo claro' : '🌙 Modo oscuro' }}
+      </button>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const darkMode = ref(false)
+
+const saludar = () => {
+  alert("Gracias por contactarme 👍")
+}
+
+const toggleTheme = () => {
+  darkMode.value = !darkMode.value
+  document.body.classList.toggle("oscuro")
+}
+</script>
