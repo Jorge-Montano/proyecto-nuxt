a5e61ffee84637c9c03c669c5e9b6e52eaab5db6
Montaño <jorge.alan.montano@gmail.com>
2026-02-20 20:19:47 -0600

    fix netlify static build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,9 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-
   css: ['~/app/css/main.css'],
 
   ssr: false,
 
   nitro: {
-    preset: 'netlify'
+    preset: 'static'
   }
 })
